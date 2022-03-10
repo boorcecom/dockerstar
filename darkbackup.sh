f199b01235b8abe6797b1a46e26f8edd93a57b36
Debian <freebox@darkstar01.ad.boorce.com>
2022-03-10 16:59:56 +0100

    Added gcloud backup

---
+++ darkbackup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+id="1iTwgwAVaHCw2kz2zk8wQimxplyxNpjX_"
+
+gdrive update 1w8AHVFtPMtpg48g1T4MWoeYJNIOjr8YI /mnt/Freebox/Docker/apache-storage.img
+gdrive update 1QXqlcuBqxKDXgCE9kmufvrKfKw_1B7hr /mnt/Freebox/Docker/mysql-storage.img
+gdrive update 191JfI69XdzQygRf20l7AtWdJCPcO_EwP /mnt/Freebox/Docker/pihole-storage.img
+
+
